dbc6f8eb414dd8e5a80ecdb81bb8fdecd2fb7fba
Nicolò Bartolini <nicolobartolinii@gmail.com>
2023-05-24 20:27:27 +0200

    Pagina di login rifatta, con tanto di fragments

--- app/src/main/java/it/univpm/filmaccio/AuthActivity.kt
+++ app/src/main/java/it/univpm/filmaccio/AuthActivity.kt
@@ -2,11 +2,25 @@ package it.univpm.filmaccio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class AuthActivity : AppCompatActivity() {
 
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+        auth = Firebase.auth
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Controlla se l'utente è già loggato
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            val pass = 0 // TODO: passa alla schermata home direttamente
+        }
     }
 }
