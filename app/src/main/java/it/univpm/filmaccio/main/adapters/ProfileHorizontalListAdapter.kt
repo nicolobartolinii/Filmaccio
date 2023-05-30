bb16946c15aa8f33363c224342203e54746ac88d
Nicolò Bartolini <nicolobartolinii@gmail.com>
2023-05-30 19:13:07 +0200

    layout della schermata di profilo

---
+++ app/src/main/java/it/univpm/filmaccio/main/adapters/ProfileHorizontalListAdapter.kt
@@ -0,0 +1,37 @@
+package it.univpm.filmaccio.main.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import it.univpm.filmaccio.R
+import it.univpm.filmaccio.data.models.ProfileListItem
+
+class ProfileHorizontalListAdapter(private val itemList: List<ProfileListItem>) :
+    RecyclerView.Adapter<ProfileHorizontalListAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.profile_list_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = itemList[position]
+        holder.bind(item)
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val titleText: TextView = itemView.findViewById(R.id.titleText)
+        private val descriptionText: TextView = itemView.findViewById(R.id.descriptionText)
+
+        fun bind(item: ProfileListItem) {
+            titleText.text = item.title
+        }
+    }
+}
