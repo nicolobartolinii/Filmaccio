9912e0073ee49f04344e638c943879e949b31896
nick <nicolobartolinii@gmail.com>
2023-06-02 10:09:05 +0200

    inizio layout MovieDetailsActivity

---
+++ app/src/main/java/it/univpm/filmaccio/details/MovieDetailsActivity.kt
@@ -0,0 +1,12 @@
+package it.univpm.filmaccio.details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import it.univpm.filmaccio.R
+
+class MovieDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+    }
+}
