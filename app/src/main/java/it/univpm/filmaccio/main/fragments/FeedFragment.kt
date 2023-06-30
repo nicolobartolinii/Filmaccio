03219e9cd21b667ca77ff7048e127ff9f559772c
Nicolò Bartolini <nicolobartolinii@gmail.com>
2023-06-30 23:58:25 +0200

    Inizio recensioni

--- app/src/main/java/it/univpm/filmaccio/main/fragments/FeedFragment.kt
+++ app/src/main/java/it/univpm/filmaccio/main/fragments/FeedFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import it.univpm.filmaccio.databinding.FragmentFeedBinding
@@ -24,6 +25,18 @@ class FeedFragment : Fragment() {
     ): View {
         _binding = FragmentFeedBinding
             .inflate(inflater, container, false)
+
+
+        binding.submitButton.setOnClickListener {
+            val n = binding.ratingBar.numStars
+            val r = binding.ratingBar.rating
+            val t = binding.ratingBar.stepSize
+            Toast.makeText(
+                requireContext(),
+                "numStars: $n, rating: $r, stepSize: $t",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
 
         return binding.root
     }
