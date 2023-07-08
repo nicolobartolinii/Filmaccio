6c7aed028a27019509c7037e327985ddc2c70031
Nicola Picciafuoco <s1099360@studenti.univpm.it>
2023-07-08 10:51:54 +0200

    tolto log

--- app/src/main/java/it/univpm/filmaccio/data/repository/SearchRepository.kt
+++ app/src/main/java/it/univpm/filmaccio/data/repository/SearchRepository.kt
@@ -12,7 +12,6 @@ class SearchRepository {
     suspend fun searchMulti(
         query: String, page: Int = 1, language: String = "it-IT", includeAdult: Boolean = false
     ): SearchResponse {
-        Log.e("Search", "Searching for $query in TMDB API")
         return tmdbApi.searchMulti(
             query = query, page = page, language = language, includeAdult = includeAdult
         )
